626e44d20a349c2071653db6b8190bd4a8e3ad8d
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-06-05 13:07:15 +0530

    Introduction to JS

---
+++ Intro.js
@@ -0,0 +1,14 @@
+console.log("Hello JS :)");
+
+let a;
+console.log(a);  // undefined
+
+a = 10;
+a = 10.1;
+
+console.log(a);
+
+a = "Hello I am a String";
+a = 'I am also a String';
+
+console.log("Value of a = ", a);
