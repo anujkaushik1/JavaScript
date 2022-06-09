7c8c2b692d0b5b5b001c1ba0fa3da6333647ed9c
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-06-09 13:32:41 +0530

    More Async Await Functions

--- AsyncAwait.js
+++ AsyncAwait.js
@@ -1,20 +1,52 @@
+const fetch = require('node-fetch');
+// console.log("Anuj Kaushik");
+
+// async function anuj(){
+//     console.log("Inside function");
+//     return "Hello";
+// }
+// let vari = anuj();
+// console.log(vari);
+// vari.then(function (data){
+//     console.log(data);
+// })
+// console.log("Before calling function");
+
+// console.log("After calling function");
+
+
+// console.log("Last line of js file");
+
+
 console.log("Anuj Kaushik");
 
 async function anuj(){
     console.log("Inside function");
-    return "Hello";
+    const response = await fetch('https://api.github.com/users')
+    console.log("before response");
+    const users = await response.json();
+    console.log('users resolved');
+
+    return users;
+
 }
-let vari = anuj();
-console.log(vari);
-vari.then(function (data){
-    console.log(data);
-})
+
 console.log("Before calling function");
+
+let a = anuj();
 
 console.log("After calling function");
 
+console.log(a);
+
+a.then(function(data){
+    console.log(data);
+})
 
 console.log("Last line of js file");
+
+
+
 
 
 
